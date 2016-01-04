367462c51cd1ad4cfe72b9ad6077f0ac3f511daa
Mathieu Savy <mathieu.savy@gmail.com>
2016-01-04 10:06:43 +0100

    Test server message on log switch on yellow to be more visible when running tests

--- test/server/server.js
+++ test/server/server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var router = express.Router();
+var chalk = require('chalk');
 
 require('./catalog/route-catalog')(router);
 require('./dataclass/route-dataclass')(router);
@@ -8,5 +9,5 @@ require('./dataclass/route-dataclass')(router);
 app.use('/rest', router);
 
 app.listen(3030, function () {
-  console.log('Test server listening on port 3030');
+  console.log(chalk.yellow('Test server listening on port 3030'));
 });
