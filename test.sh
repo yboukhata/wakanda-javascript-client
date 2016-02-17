bea372300a992e292f0044e7994884f3d10641fd
Mathieu Savy <mathieu.savy@gmail.com>
2016-02-17 09:32:04 +0100

    Edit test.sh script

--- test.sh
+++ test.sh
@@ -1,11 +1,21 @@
-#/bin/bash
+#!/bin/bash
 
 npm run test-server:start
 
 if [[ "$1" != "single" ]]
 then
   npm run webpack-build
+  RESULT=$?
+
+  if [[ RESULT -gt 0 ]]
+  then
+    exit $RESULT
+  fi
 fi
 
 npm run test:node:single && npm run test:karma:single
+RESULT=$?
+
 npm run test-server:stop
+
+exit $RESULT
