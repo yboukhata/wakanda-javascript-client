f43ec4dfa6e295fa0c03baeb7286c74e2415b46b
Mathieu Savy <mathieu.savy@gmail.com>
2016-04-13 16:48:13 +0200

    Refactor collection service with base one

--- src/data-access/service/collection-service.ts
+++ src/data-access/service/collection-service.ts
@@ -4,6 +4,7 @@ import Collection from '../../presentation/collection';
 import {DataClass} from '../../presentation/dataclass';
 import {QueryOption} from '../../presentation/query-option';
 import {CollectionDBO} from '../../business/collection-business';
+import {CollectionBaseService, isEntitySetUri} from './base/collection-base-service';
 
 class CollectionService extends AbstractService {
   
@@ -18,83 +19,36 @@ class CollectionService extends AbstractService {
     this.collection = collection;
     this.dataClass = dataClass;
     this.collectionUri = collectionUri;
-    this.isEntitySet = this._isEntitySetUri({uri: collectionUri});
+    this.isEntitySet = isEntitySetUri(collectionUri);
   }
 
   fetch(options: QueryOption): Promise<CollectionDBO> {
 
-    if (!this.isEntitySet) {
-      if(options.select && options.select.length > 0) {
-        throw new Error('Collection.fetch: option select is not allowed when collection is deferred');
-      }
-    }
-
-    options.method = 'subentityset';
-
-    var optString = Util.handleOptions(options);
-
-    //Remove the first ? on optString if it's not an entitySet (because there is also
-    //?$expand=... on collectionUri), and add a &
-    if (!this.isEntitySet) {
-      optString = '&' + optString.slice(1);
-    }
-
-    //Remove the /rest/ part of the URI as our service will add it on its own
-    // let uri = this.collectionUri.slice(5);
-    let uri = this._removeRestFromUri(this.collectionUri);
-
-    return this.httpClient.get({
-      uri: uri + optString
-    }).then(res => {
-      let obj = JSON.parse(res.body);
-
-      if (obj.__ENTITYSET) {
-        this.collectionUri = obj.__ENTITYSET;
-        this.isEntitySet = this._isEntitySetUri({uri: obj.__ENTITYSET});
-      }
-
-      delete obj.__entityModel;
-
-      for (let entity of obj.__ENTITIES) {
-        Util.removeRestInfoFromEntity(entity);
-      }
-
-      return obj;
-    });
+    return CollectionBaseService.fetch({
+      httpClient: this.httpClient,
+      collectionUri: this.collectionUri,
+      isEntitySet: this.isEntitySet,
+      options
+    })
+      .then(dbo => {
+        
+        if (dbo.__ENTITYSET) {
+          this.collectionUri = dbo.__ENTITYSET;
+          this.isEntitySet = isEntitySetUri(dbo.__ENTITYSET);
+        }
+        
+        return dbo;
+      });
   }
   
   public callMethod(methodName: string, parameters: any[]): Promise<any> {
-    //Two cases. If it's an entity set, just call the method
-    //If not, call it with emMethod and subentityset parameters
-    let uri = this._removeRestFromUri(this.collectionUri);
-    
-    if (this.isEntitySet) {
-      uri += '/' + methodName;
-    }
-    else {
-      let optString = Util.handleOptions({
-        method: 'subentityset',
-        emMethod: methodName
-      });
-      
-      uri += '&' + optString.slice(1);
-    }
-    
-    return this.httpClient.post({
-        uri,
-        data: parameters
-      }).then(res => {
-        let obj = JSON.parse(res.body);
-        return obj.result || obj || null;
-      });
-  }
-  
-  private _removeRestFromUri(uri: string): string {
-    return uri.slice(5);
-  }
-
-  _isEntitySetUri({uri}: {uri: string}): boolean {
-    return /^\/rest\/\w+\/\$entityset\/[A-Z0-9]+(\?.*)?$/i.test(uri);
+    return CollectionBaseService.callMethod({
+      httpClient: this.httpClient,
+      collectionUri: this.collectionUri,
+      isEntitySet: this.isEntitySet,
+      methodName,
+      parameters
+    });
   }
 }
 
