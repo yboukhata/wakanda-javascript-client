c60d60053f54ceff58b5f20b77b8d96c2fe6f393
Mathieu Savy <mathieu.savy@gmail.com>
2016-07-01 15:03:29 +0200

    Add madkeeper to handle empty dataStore case

--- src/data-access/service/base/catalog-base-service.ts
+++ src/data-access/service/base/catalog-base-service.ts
@@ -21,36 +21,38 @@ export class CatalogBaseService {
         let catalog: IDataClassDBO[] = [];
         let rawObj = JSON.parse(res.body);
 
-        for (let d of rawObj.dataClasses) {
+        if (rawObj.dataClasses) {
+          for (let d of rawObj.dataClasses) {
 
-          let attributes: any[] = [];
-          if (d.attributes) { //Seriously? :)
-            for (let attr of d.attributes) {
-              attributes.push({
-                name: attr.name,
-                kind: attr.kind,
-                type: attr.type,
-                readOnly: attr.readOnly
-              });
+            let attributes: any[] = [];
+            if (d.attributes) { //Seriously? :)
+              for (let attr of d.attributes) {
+                attributes.push({
+                  name: attr.name,
+                  kind: attr.kind,
+                  type: attr.type,
+                  readOnly: attr.readOnly
+                });
+              }
             }
-          }
 
-          let methods: any[] = [];
-          if (d.methods) {
-            for (let m of d.methods) {
-              methods.push({
-                name: m.name,
-                applyTo: m.applyTo
-              });
+            let methods: any[] = [];
+            if (d.methods) {
+              for (let m of d.methods) {
+                methods.push({
+                  name: m.name,
+                  applyTo: m.applyTo
+                });
+              }
             }
-          }
 
-          catalog.push({
-            name: d.name,
-            collectionName: d.collectionName,
-            attributes,
-            methods
-          });
+            catalog.push({
+              name: d.name,
+              collectionName: d.collectionName,
+              attributes,
+              methods
+            });
+          }
         }
 
         return catalog;
