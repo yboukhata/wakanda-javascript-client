854008b32163aa1adf542aff30dcc24eeca04a85
yboukhata <yacine_r@yahoo.fr>
2016-11-04 14:12:16 +0100

    code indentation

--- src/business/util.ts
+++ src/business/util.ts
@@ -3,12 +3,12 @@ class Util {
   public static wakParseSimpleDate(stringDate: string): Date {
     // In wakanda, simple date is a date with only year, month and hour
     // in this format : DD!MM!YYYY
-    if (! stringDate) {
+    if (!stringDate) {
       return null;
     }
 
     let arr = stringDate.split('!');
-    if(arr.length !== 3) {
+    if (arr.length !== 3) {
       // return null or throw an error, simple date format is ko
       return null;
     }
@@ -19,8 +19,8 @@ class Util {
   public static wakToStringSimpleDate(date: Date): String {
     let wakSimpleDate: String;
 
-    if(!(date instanceof Date)) {
-      return null
+    if (!(date instanceof Date)) {
+      return null;
     }
 
     wakSimpleDate = date.getUTCDate() + '!' + (date.getUTCMonth() + 1) + '!' + date.getUTCFullYear();
